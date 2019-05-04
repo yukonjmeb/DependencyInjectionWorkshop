59fc27cf69363e4ac4caab7a3b7a1b8f2c57adaa
jane.li93 <jane.li@titansoft.com.sg>
2019-05-04 10:07:17 +0800

    first step

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,6 +1,50 @@
 ﻿namespace DependencyInjectionWorkshop.Models
 {
+    using System;
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Text;
+
+    using Dapper;
+
     public class AuthenticationService
     {
+
+        public bool Verify(string accountId, string password, string otp)
+        {
+            var DBPassword = string.Empty;
+            var CurrentOTP = string.Empty;
+
+            using (var connection = new SqlConnection("my connection string"))
+            {
+                DBPassword = connection.Query<string>("spGetUserPassword", new { Id = accountId },
+                    commandType: CommandType.StoredProcedure).SingleOrDefault();
+            }
+
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new StringBuilder();
+            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password));
+            foreach (var theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+
+            var HashPassword = hash.ToString();
+
+            var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
+            var response = httpClient.PostAsJsonAsync("api/otps", accountId).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                CurrentOTP = response.Content.ReadAsAsync<string>().Result;
+            }
+            else
+            {
+                throw new Exception($"web api error, accountId:{accountId}");
+            }
+
+            return (DBPassword == HashPassword && CurrentOTP == otp);
+        }
     }
 }
